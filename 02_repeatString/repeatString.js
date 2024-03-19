10d54c775e3be7b662eae634869ee22e0205b8dc
areaboxer <jamicojerahmeel@gmail.com>
2024-03-19 19:11:48 +0100

    upadete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+  if (num < 0) return "error";
+  let string = "";
+  for (let i = 0; i < num; i++) {
+    string += word;
+  }
+  return string;
 };
+// const repeatString = function (str, num) {
+//     if (num < 0) return "error";
+//     return str.repeat(num);
+//   };
 
 // Do not edit below this line
 module.exports = repeatString;
