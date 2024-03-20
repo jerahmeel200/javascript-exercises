41ed86ad065a2c890cf480ec3b095faa5e0b673a
areaboxer <jamicojerahmeel@gmail.com>
2024-03-20 08:36:10 +0100

    upadete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let splitedString = str.split("");
+  let reverseArray = splitedString.reverse();
+  let reversedString = reverseArray.join("");
 
+  return reversedString;
 };
 
 // Do not edit below this line
